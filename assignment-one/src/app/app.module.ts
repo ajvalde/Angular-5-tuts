c26244a85439a9ae6524dbe95c52f0bfeda42300
Anthony <anthonyguitarman13@gmail.com>
2018-04-24 14:52:44 -0600

    inital commit

---
+++ assignment-one/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { SuccessComponent } from './success/success.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SuccessComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
